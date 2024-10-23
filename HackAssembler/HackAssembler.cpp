cc1578b1d8ac33627cd6c14fef83ead011144f3a
Brody Tubbs <btubbs200@gmail.com>
2024-10-23 10:56:21 -0500

    Add comment deletion and prepare label/instruction parsing

--- HackAssembler/HackAssembler.cpp
+++ HackAssembler/HackAssembler.cpp
@@ -1,51 +1,75 @@
-// HackAssembler.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-#include "Destinations.h";
-#include "Jumps.h";
-#include "Computations.h";
-#include "Bin2Hex.h";
+#include "Destinations.h"
+#include "Jumps.h"
+#include "Computations.h"
+#include "Bin2Hex.h"
 #include <iostream>
-#include <fstream>;
-#include <string>;
-#include <map>;
+#include <fstream>
+#include <string>
+#include <map>
+#include <vector>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	/*
-	Destinations dstTbl;
-	Computations compTbl;
-	Jumps jmpTbl;
-	Bin2Hex bin2HexTbl;
-
-	cout << dstTbl.find("MD") << endl;
-	cout << compTbl.find("0") << endl;
-	cout << jmpTbl.find("JGE") << endl;
-	cout << bin2HexTbl.find("1010") << endl;
-	cout << bin2HexTbl.Convert16Bin2Hex("1010111111000101");
-	*/
-
 	string inFileName, outFileName, inStr;
+	vector<string> instructions;
+	map<string, int> labels;
+	map<string, int>::iterator itLabels;
+	int lineCount = 0;
 	
-	if (argc < 2)
+	if (argc < 2) //user provided one or no file names
 	{
 		cout << "You must enter two parameters, input file and output file names." << endl;
 	}
 	else
 	{
-		//find user-specified .asm file in C:\\mydata
+		//get file names from command line arguments
 		inFileName = "C:\\mydata\\" + string(argv[1]) + ".asm";
-
-		//create user-specified name for save file in C:\\mydata
 		outFileName = "C:\\mydata\\" + string(argv[2]);
 	}
 
-	ifstream inFile(inFileName);
+	ifstream inFile(inFileName); //open input file
 
-	//iterate through all items in file
-	while (getline(inFile, inStr, '\r'))
+	//iterate through & display all items in input file
+	while (getline(inFile, inStr, '\n'))
 	{
-		cout << inStr << endl;
+		int commentLoc = inStr.find("//");
+
+		//delete comments
+		if (commentLoc != string::npos)
+		{
+			inStr = inStr.substr(0, commentLoc);
+		}
+
+		if (inStr.size() == 0)
+		{
+			continue;
+		}
+
+		//parse labels
+		if (inStr.find("(") != string::npos && inStr.find(")") != string::npos)
+		{
+			int leftLoc = inStr.find("("); int rightLoc = inStr.find(")");
+			string label = inStr.substr(leftLoc + 1, rightLoc - (leftLoc + 1));
+
+			itLabels = labels.find(label);
+
+			//if label doesn't already exist, add to labels map
+			if (itLabels == labels.end())
+			{
+				labels.insert(pair<string, int>(label, lineCount));
+			}
+			else
+			{
+				cout << "Duplicate Label at Line: " + lineCount;
+			}
+		}
+		//save label to instructions map
+		else
+		{
+			instructions.push_back(inStr);
+			lineCount++;
+		}
 	}
 }
